2ba364189fc13624cd45e309d53797599bcdb28a
Joel Romero <joelrg1288@gmail.com>
2017-06-15 15:01:34 -0500

    Correccion de interfaz e implementacion de formulas de sensores

---
+++ Proyecto/PicWinUSB/PicWinUSB_SRC/Modificado/Intrumentacion/DeviceInterfaceImports.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+
+namespace Intrumentacion
+{
+    class DeviceInterfaceImports
+    {
+        public const int DIGCF_PRESENT = 0x00000002;
+        public const int DIGCF_DEVICEINTERFACE = 0x00000010;
+
+        public const int FILE_ATTRIBUTE_NORMAL = 0x00000080;
+        public const int FILE_FLAG_OVERLAPPED = 0x40000000;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SP_DEVICE_INTERFACE_DATA
+        {
+            public int cbSize;
+            public Guid InterfaceClassGuid;
+            public int Flags;
+            public IntPtr Reserved;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SP_DEVICE_INTERFACE_DETAIL_DATA
+        {
+            public int cbSize;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string DevicePath;
+        }
+
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetupDiGetClassDevs(
+            ref Guid ClassGuid,
+            [MarshalAs(UnmanagedType.LPTStr)] string Enumerator,
+            IntPtr hwndParent,
+            UInt32 Flags);
+
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Boolean SetupDiEnumDeviceInterfaces(
+            IntPtr hDevInfo,
+            IntPtr devInfo,
+            ref Guid interfaceClassGuid,
+            UInt32 memberIndex,
+            ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData);
+
+        [DllImport(@"setupapi.dll", SetLastError = true)]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(
+            IntPtr hDevInfo,
+            ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData,
+            IntPtr deviceInterfaceDetailData,
+            UInt32 deviceInterfaceDetailDataSize,
+            out UInt32 requiredSize,
+            IntPtr deviceInfoData);
+
+        [DllImport(@"setupapi.dll", SetLastError = true)]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(
+            IntPtr hDevInfo,
+            ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData,
+            ref SP_DEVICE_INTERFACE_DETAIL_DATA deviceInterfaceDetailData,
+            UInt32 deviceInterfaceDetailDataSize,
+            out UInt32 requiredSize,
+            IntPtr deviceInfoData);
+
+        [DllImport(@"setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern UInt16 SetupDiDestroyDeviceInfoList(
+            IntPtr hDevInfo);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateFile(
+            string fileName,
+            FileAccess fileAccess,
+            FileShare fileShare,
+            IntPtr securityAttributes,
+            FileMode creationDisposition,
+            UInt32 flags,
+            IntPtr template);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(
+            IntPtr hObject);
+    }
+}
---
+++ Proyecto/PicWinUSB/PicWinUSB_SRC/Modificado/Intrumentacion/operaciones.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intrumentacion
+{
+    class operaciones
+    {
+       public IntPtr iHandle;                             // global device handle definition
+       public PicWinUSBAPI picwinusbapi = new PicWinUSBAPI();
+      // public Int16 x = 0;
+
+        public operaciones()
+        {
+
+            Guid InterfaceGuid = new Guid("31415926-5358-9793-2384-626433832795"); // .Inf defined Guid                        
+            iHandle = picwinusbapi.Init_PicWinUSB(InterfaceGuid);
+        }
+
+        public byte suma(double numero_1, double numero_2)
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];           // Define send DataBuffer size
+            byte[] rdBuffer = new byte[1];           // Define recieve DataBuffer size
+
+            sdBuffer[0] = 0x00;    //Modo SUMA
+            sdBuffer[1] = Convert.ToByte(numero_1);
+            sdBuffer[2] = Convert.ToByte(numero_2);
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer);
+            bres = picwinusbapi.Read_PicWinUSB(iHandle, rdBuffer);
+            return rdBuffer[0];
+
+        }
+
+        public byte resta(double numero_1, double numero_2)
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];           // Define send DataBuffer size
+            byte[] rdBuffer = new byte[1];           // Define recieve DataBuffer size
+
+            sdBuffer[0] = 0x01;    //Modo RESTA
+            sdBuffer[1] = Convert.ToByte(numero_1);
+            sdBuffer[2] = Convert.ToByte(numero_2);
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer);
+            bres = picwinusbapi.Read_PicWinUSB(iHandle, rdBuffer);
+            return rdBuffer[0];
+        }
+
+        public byte div(double numero_1, double numero_2)
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];           // Define send DataBuffer size
+            byte[] rdBuffer = new byte[1];           // Define recieve DataBuffer size
+
+            sdBuffer[0] = 0x03;    //Modo DIVISION
+            sdBuffer[1] = Convert.ToByte(numero_1);
+            sdBuffer[2] = Convert.ToByte(numero_2);
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer);
+            bres = picwinusbapi.Read_PicWinUSB(iHandle, rdBuffer);
+            return rdBuffer[0];
+        }
+
+        public byte multi(double numero_1, double numero_2)
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];           // Define send DataBuffer size
+            byte[] rdBuffer = new byte[1];           // Define recieve DataBuffer size
+
+            sdBuffer[0] = 0x02;    //Modo MULTIPLICACION
+            sdBuffer[1] = Convert.ToByte(numero_1);
+            sdBuffer[2] = Convert.ToByte(numero_2);
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer);
+            bres = picwinusbapi.Read_PicWinUSB(iHandle, rdBuffer);
+            return rdBuffer[0];
+
+        }
+
+        public void led_verde_on()
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];                          // Definer el tamaño del buffer
+            sdBuffer[0] = 0X09;                                     //Modo Led
+            sdBuffer[1] = 0X01;                                     //Led ROJO OFF Led Verde ON
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer); //Enviar
+
+        }
+
+        public void led_rojo_on()
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];                          // Definer el tamaño del buffer
+            sdBuffer[0] = 0X09;                                     //Modo Led
+            sdBuffer[1] = 0X02;                                     //Led ROJO ON Led Verde OFF
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer); //Enviar
+        }
+
+        public void leds_on()
+        {
+
+            bool bres;
+            byte[] sdBuffer = new byte[3];                          // Definer el tamaño del buffer
+            sdBuffer[0] = 0X09;                                     //Modo Led
+            sdBuffer[1] = 0X03;                                     //Leds ON
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer); //Enviar
+
+        }
+
+        public void leds_off()
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];                          // Definer el tamaño del buffer
+            sdBuffer[0] = 0X09;                                     //Modo Led
+            sdBuffer[1] = 0X00;                                     //Leds OFF
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer); //Enviar
+        }
+
+        public void leds_toggle()
+        {
+            bool bres;
+            byte[] sdBuffer = new byte[3];                          // Definer el tamaño del buffer
+            sdBuffer[0] = 0X09;                                     //Modo Led
+            sdBuffer[1] = 0X04;                                     //Led Toggle
+            bres = picwinusbapi.Write_PicWinUSB(iHandle, sdBuffer); //Enviar
+        }
+    }
+}
